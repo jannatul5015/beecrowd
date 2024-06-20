41872a6963298c6a4a0436ac7be5bc331f1d8d15
Jannatul Ferdous <jj.cuet.cse.edu@gmail.com>
2024-06-20 23:05:31 +0600

    Create 1197.c

---
+++ 1197.c
@@ -0,0 +1,16 @@
+// 1097 - Sequence IJ 3
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    for (int i = 1; i<= 9; i += 2)
+    {
+        for (int j = i+6; j >= i+4; j--)
+        {
+            printf("I=%d J=%d\n", i, j);
+        }
+    }
+ 
+    return 0;
+}
